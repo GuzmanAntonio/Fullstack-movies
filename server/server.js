d47fc9320daea41a52712e20f37849a33889ff6d
Antonio Guzman <alguzman1@yahoo.com>
2017-12-01 10:45:02 -0500

    FullStack: Fist App

--- server/server.js
+++ server/server.js
@@ -15,22 +15,59 @@ app.use(require('./config/error-handler'))
 
 const ourFirstMovie = {title: 'Jaws', year: 1965}
 
-Movie(ourFirstMovie).save((err, movie) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('MOVIE ADDED TO DB', movie)
-  }
+//const createMovie = () => {
+app.post('/api/movies', (req, res) => {
+
+  const {title, year} = req.body
+  const newMovie = {title, year}
+
+  Movie(newMovie).save((err, savedMovie) => {
+    if (err) {
+      res.json({error: err})
+    } else {
+      res.json({msg: 'SUCCESS', data: savedMovie})
+    }
+  })
 })
 
-Movie.find((err, movies) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('****** HERES YOUR MOVIES ******', movies)
-  }
+//createMovie()
+
+app.get('/api/movies', (req, res) => {
+  Movie.find((err, movies) => {
+    if (err) {
+      res.json({error: err})
+    } else {
+      res.json({msg: 'success', movies})
+    }
+  })
 })
 
-const server = app.listen(port, () => console.log(`Running on port: ${port}`))
+app.post('/api/movies/:movies_id', (req, res) => {
+  const movieId = req.params.movieId
+  Movie.findById({_id: movieId}, (err, movie) => {
+    if(err) {
+      res.json({error: err})
+    } else {
+      res.json({msg: `Found ${movie.title}`, movie: movie})
+    }
+  //res.json({ data: req.params.movie_id})
+  })
+})
+
+app.get('/api/random-movie', (req, res) => {
+  // Return a random movie from the DB
+  // Have it return all movies first
+  Movie.find((err, data) => {
+    if (err){
+      res.json({error: err})
+    } else {
+      const allMoviesArray = data
+      const randomItem = allMoviesArray[Math.floor(Math.random() * allMoviesArray.length)]
+      res.json({msg: 'Random Item Retrived', data: randomItem})
+    }
+  })
+})
+
+const server = app.listen(port, () => console.log(`Running on port: ${port} 💩`))
 
 module.exports = server
